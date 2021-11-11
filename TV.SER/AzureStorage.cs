9d46304ad56484fbe18a29acb36e19ecab0d2c49
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2021-11-10 22:52:41 -0300

    Refatorado service para dar resize nos arquivos ao subir no storage

--- TV.SER/AzureStorage.cs
+++ TV.SER/AzureStorage.cs
@@ -4,6 +4,13 @@ using System.Threading.Tasks;
 using TV.SER.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.Storage.Blob;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using System.Text.RegularExpressions;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
 
 namespace TV.SER
 {
@@ -14,8 +21,8 @@ namespace TV.SER
         public AzureStorage(IStorageConnectionFactory storageConnectionFactory)
         {
             _storageConnectionFactory = storageConnectionFactory;
-
         }
+
         public async Task DeleteImage(string name)
         {
             Uri uri = new Uri(name);
@@ -28,17 +35,70 @@ namespace TV.SER
         public async Task<string> UploadAsync(IFormFile file)
         {
             var blobContainer = await _storageConnectionFactory.GetContainer();
+            var thumbnailWidth = 100;
+            var extension = Path.GetExtension(file.FileName);
+            var encoder = GetEncoder(extension);
             CloudBlockBlob blob = blobContainer.GetBlockBlobReference(GetRandomBlobName(file.FileName));
             using (var stream = file.OpenReadStream())
             {
-                await blob.UploadFromStreamAsync(stream);
+                using (var output = new MemoryStream())
+                using (Image image = Image.Load(stream))
+                {
+                    var divisor = image.Width / thumbnailWidth;
+                    var height = Convert.ToInt32(Math.Round((decimal)(image.Height / divisor)));
+
+                    image.Mutate(x => x.Resize(thumbnailWidth, height));
+                    image.Save(output, encoder);
+                    output.Position = 0;
+                    await blob.UploadFromStreamAsync(output);
+                }
             }
+
             return blob.Uri.AbsoluteUri;
+
         }
+
+        /// <summary> 
+        /// string GetRandomBlobName(string filename): Generates a unique random file name to be uploaded  
+        /// </summary> 
         private string GetRandomBlobName(string filename)
         {
             string ext = Path.GetExtension(filename);
             return string.Format("{0:10}_{1}{2}", DateTime.Now.Ticks, Guid.NewGuid(), ext);
         }
+
+        private static IImageEncoder GetEncoder(string extension)
+        {
+            IImageEncoder encoder = null;
+
+            extension = extension.Replace(".", "");
+
+            var isSupported = Regex.IsMatch(extension,
+                                            "gif|png|jpe?g",
+                                            RegexOptions.IgnoreCase);
+
+            if (isSupported)
+            {
+                switch (extension)
+                {
+                    case "png":
+                        encoder = new PngEncoder();
+                        break;
+                    case "jpg":
+                        encoder = new JpegEncoder();
+                        break;
+                    case "jpeg":
+                        encoder = new JpegEncoder();
+                        break;
+                    case "gif":
+                        encoder = new GifEncoder();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return encoder;
+        }
     }
-}
+} 
