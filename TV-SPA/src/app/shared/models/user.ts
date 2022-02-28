2c033621ce718acb7195063b86b1b1270a3528cc
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-02-28 17:41:55 -0300

    separado repositórios back e front

--- TV-SPA/src/app/shared/models/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-    id: string;
-    username : string;
-    email : string;
-    profileImageUrl?: string;
-    name : string;
-    adress: string;
-    federalRegistration: string;
-}
